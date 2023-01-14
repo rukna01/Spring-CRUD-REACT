b98505d6a11685ccac3015f2e404599bfb6a5109
rukna01 <ankurjaiswal19999@gmail.com>
2023-01-15 02:11:19 +0530

    Adding basic spring dependency file

---
+++ projectFirst/src/main/java/com/projectFirst/projectFirst/ProjectFirstApplication.java
@@ -0,0 +1,13 @@
+package com.projectFirst.projectFirst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectFirstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectFirstApplication.class, args);
+	}
+
+}
---
+++ projectFirst/src/test/java/com/projectFirst/projectFirst/ProjectFirstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectFirst.projectFirst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectFirstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
